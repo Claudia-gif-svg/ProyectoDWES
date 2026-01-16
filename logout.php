6ebb61eeabafa383b6983e0e1b13ae5118e1b110
Claudia Rodriguez  Corrochano <claudia.rodriguezcorrochano@riberadeltajo.es>
2026-01-16 17:45:26 +0100

    Implement logout functionality

--- logout.php
+++ logout.php
@@ -1,1 +1,12 @@
+<?php
+session_start();
 
+// Vaciar todas las variables de sesión
+$_SESSION = [];
+
+// Destruir la sesión
+session_destroy();
+
+// Redirigir al login
+header("Location: index.php");
+exit;
