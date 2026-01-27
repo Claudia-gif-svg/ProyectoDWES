d6200ed5403242e83165034bf1ceafd6ed6b06a5
diegogonzaleztoledano-12 <diego.gonzaleztoledano@riberadeltajo.es>
2026-01-27 18:08:24 +0100

    Update admin redirect path in index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $_SESSION["rol"] = $usuarioBD["rol"];
 
         if ($usuarioBD["rol"] === "administrador") {
-            header("Location: admin.php");
+            header("Location: pages/admin.php");
         } else {
             header("Location: pages/usuario.php");
         }
